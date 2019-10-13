5844653a0214790e6090eea559064d35a9c6b0d8
Chris Puzzuoli <cpuzzuol@umich.edu>
2019-10-13 15:05:53 -0400

    set up vuetify 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Vuetify = require('vuetify');
+Vue.use(Vuetify)
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +22,6 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +30,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify()
 });
