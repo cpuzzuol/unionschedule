1ccd550d31ea4977fa1a2b6d7acd6c9eb5a06eee
Chris Puzzuoli <cpuzzuol@umich.edu>
2019-10-14 22:33:10 -0400

    aws eb instance goes to 'severe' status if redirects are broken (welcome view was commented out and / route was under auth control)

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 //
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', 'AdminController@index')->name('adminIndex');
